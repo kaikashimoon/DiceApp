05bd30eb8da6c83c6d7e39b171aeaf2d3450cffe
kai <leonc4667@gmail.com>
2022-09-05 12:16:42 -0400

    All complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'dart:developer' as developer;
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -6,20 +8,66 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
-          title: const Text('Dice'),
+          title: const Center(child: Text('Dicee')),
           backgroundColor: Colors.red,
         ),
-        body: const DicePage(),
+        body: DicePage(),
       ),
     ),
   );
 }
 
-class DicePage extends StatelessWidget {
-  const DicePage({super.key});
+class DicePage extends StatefulWidget {
+  const DicePage({Key? key}) : super(key: key);
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rigthDiceNumber = 1;
+
+  void getDiceNumber() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rigthDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: TextButton(
+              style:
+                  TextButton.styleFrom(splashFactory: NoSplash.splashFactory),
+              onPressed: () {
+                getDiceNumber();
+                developer.log('Button pressed 1');
+              },
+              child: Image.asset(
+                ('images/dice$leftDiceNumber.png'),
+              ),
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+              style:
+                  TextButton.styleFrom(splashFactory: NoSplash.splashFactory),
+              onPressed: () {
+                getDiceNumber();
+                developer.log('Button pressed 2');
+              },
+              child: Image.asset(
+                ('images/dice$rigthDiceNumber.png'),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
